b297e424bf3055fe8b1558bb3a01fd510c6f58ea
Stephan2801 <stephan.reiter.2801@gmail.com>
2019-02-15 20:23:17 +0100

    Loading map

--- Farmlife/Assets/Farmlife.cs
+++ Farmlife/Assets/Farmlife.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Farmlife : MonoBehaviour
 {
-    private Terrain Terrain;
+    public Grid map;
+    private Dictionary<string, Sprite> sprites;
 
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("Initializing ...");
+
+        LoadMap();
+
         Debug.Log("Initialized");
     }
 
@@ -20,8 +26,43 @@ public class Farmlife : MonoBehaviour
 
 
 
+    private void LoadSprites()
+    {
+        Debug.Log("Loading sprites ...");
+
+        sprites = new Dictionary<string, Sprite>();
+
+        Debug.Log("grass");
+        
+        sprites.Add("grass", Resources.Load<Sprite>("Sprites/Ground/grass"));
+
+        Debug.Log("Loaded sprites!");
+    }
+
+
+
     private void LoadMap()
     {
+        LoadSprites();
+
+        Debug.Log("Getting reference to ground");
+        Tilemap ground = map.transform.Find("Ground").gameObject.GetComponent<Tilemap>();
+        //Tilemap bushes = map.transform.Find("Bushes").gameObject.GetComponent<Tilemap>();
+        //Tilemap fields = map.transform.Find("Fields").gameObject.GetComponent<Tilemap>();
+        //Tilemap crops = map.transform.Find("Crops").gameObject.GetComponent<Tilemap>();
+        //Tilemap pavement = map.transform.Find("Pavement").gameObject.GetComponent<Tilemap>();
+        //Tilemap structures = map.transform.Find("Structures").gameObject.GetComponent<Tilemap>();
+
+
+        Debug.Log("Creating do");
+        Tile grass = (Tile)Tile.CreateInstance("Tile");
+
         
+
+        grass.sprite = sprites["grass"];
+
+        Vector3Int position = new Vector3Int(0,0,0);
+
+        ground.SetTile(position, grass);
     }
 }
