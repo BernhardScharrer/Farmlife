cc9deae180cdf3f7e9bf3f9c0cbc1fa9193a0651
Stephan2801 <stephan.reiter.2801@gmail.com>
2019-02-21 18:36:44 +0100

    Further server communication and login message creation

---
+++ Farmlife/Assets/Scripts/ServerComm.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Net.Http;
+using System.Text;
+
+public class ServerComm : MonoBehaviour
+{
+    public static HttpClient client { get; private set; }
+    public static string lastResponse { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        client = new HttpClient();
+    }
+
+    public static async void Post(string target, string message)
+    {
+        HttpContent content = new StringContent(message, Encoding.UTF8, "application/json");
+
+        //HttpResponseMessage responseMessage = await client.PostAsync(target, content);
+
+        //lastResponse = await responseMessage.Content.ReadAsStringAsync();
+
+        lastResponse = "true";
+    }
+
+    public static async void Get(string target)
+    {
+        //lastResponse = await client.GetStringAsync(target);
+    }
+}
--- Farmlife/Assets/Scripts/btnLogin_Click.cs
+++ Farmlife/Assets/Scripts/btnLogin_Click.cs
@@ -5,48 +5,56 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 // for server communication
-using System.Net;
 using System.Net.Http;
+using System.Text;
 
 public class btnLogin_Click : MonoBehaviour
 {
     public Button btnLogin;
 
-    public Text txtUsername;
-    public Text txtPassword;
+    public InputField txtUsername;
+    public InputField txtPassword;
 
-    private HttpWebRequest serverAddress;
+    private string postTarget = "https://fforganizer.at/farmlife/login.php";
     
 
     // Start is called before the first frame update
     void Start()
     {
+
+        //	-----------------------------------------------
+        //	Add listener for Login button
+        //	-----------------------------------------------
         Debug.Log("Adding listener for login button ...");
 
         btnLogin.onClick.AddListener(btnLogin_OnClick);
 
         Debug.Log("Added listener for login button!");
-
-        serverAddress = HttpWebRequest.CreateHttp("https://fforganizer.at/farmlife/login.php");
     }
 
     // Update is called once per frame
     void Update()
     {
+        ServerComm.Get(postTarget);
 
+        if (ServerComm.lastResponse == "true")
+        {
+            Debug.Log("Loading new scene ...");
+
+            SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+        }
     }
 
     void btnLogin_OnClick()
     {
-        Debug.Log("Loading new scene ...");
+        string message = "{\n";
+        message += "\"Username\": \"" + txtUsername.text + "\",\n";
+        message += "\"Password\": \"" + txtPassword.text + "\"\n";
+        message += "}";
 
-        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
 
-        Debug.Log("Scene loaded!");
-    }
+        Debug.Log("Sending username and password to server:\n" + message);
 
-    void sendLoginData()
-    {
-        
+        ServerComm.Post(postTarget, message);
     }
 }
