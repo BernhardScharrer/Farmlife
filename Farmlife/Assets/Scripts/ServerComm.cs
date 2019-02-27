29482d5826244e62c5da4d66e44a66786d3a0e57
Stephan2801 <stephan.reiter.2801@gmail.com>
2019-02-27 19:27:55 +0100

    ServComm improved including json library

--- Farmlife/Assets/Scripts/ServerComm.cs
+++ Farmlife/Assets/Scripts/ServerComm.cs
@@ -9,6 +9,7 @@ public class ServerComm : MonoBehaviour
 {
     public static HttpClient client { get; private set; }
     public static string lastResponse { get; private set; }
+    private static string postTarget = "https://fforganizer.at/farmlife/farmlife.php";
 
     // Start is called before the first frame update
     void Start()
@@ -16,19 +17,37 @@ public class ServerComm : MonoBehaviour
         client = new HttpClient();
     }
 
-    public static async void Post(string target, string message)
+    public static async void Post(string message)
     {
         HttpContent content = new StringContent(message, Encoding.UTF8, "application/json");
 
-        //HttpResponseMessage responseMessage = await client.PostAsync(target, content);
+        HttpResponseMessage responseMessage = await client.PostAsync(postTarget, content);
 
-        //lastResponse = await responseMessage.Content.ReadAsStringAsync();
+        Debug.Log(responseMessage.StatusCode);
 
-        lastResponse = "true";
+        lastResponse = await responseMessage.Content.ReadAsStringAsync();
     }
 
-    public static async void Get(string target)
+    public static async void Get()
     {
-        //lastResponse = await client.GetStringAsync(target);
+        lastResponse = await client.GetStringAsync(postTarget);
     }
+}
+
+public class Login
+{
+    public string Username;
+    public string Password;
+
+    public Login()
+    {
+
+    }
+
+    public Login(string username, string password)
+    {
+        Username = username;
+        Password = password;
+    }
+
 }
